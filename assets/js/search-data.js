fbd1de65711a2033cab9cf54c4b72bd647117566
gserrera <guillermo.serrera@unican.es>
2025-11-02 21:28:49 +0000

    Deploying to gh-pages from @ gserrera/gserrera.github.io@6af83d0f2b4acf63c1a80e6be2eaa8988f3176ae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,7 +55,12 @@ ninja.data = [{
       },{id: "news-website-launched-sparkles",
           title: 'Website launched! :sparkles:',
           description: "",
-          section: "News",},{id: "projects-project-3-with-very-long-name",
+          section: "News",},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {

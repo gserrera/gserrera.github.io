d5855826c38a2d48c89ed824b7d6a93c5b4da3ca
gserrera <guillermo.serrera@unican.es>
2026-07-08 18:41:08 +0000

    Deploying to gh-pages from @ gserrera/gserrera.github.io@1ffee86f992fea53a87a92ccb870b97b9f91c3fe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,9 @@ ninja.data = [{
           section: "News",},{id: "news-article-published-in-advanced-functional-materials",
           title: 'Article published in Advanced Functional Materials !',
           description: "",
-          section: "News",},{id: "news-preprint-manuscript-added",
+          section: "News",handler: () => {
+              window.location.href = "/news/ColorRouting/";
+            },},{id: "news-preprint-manuscript-added",
           title: 'Preprint manuscript added!',
           description: "",
           section: "News",},{id: "news-manuscripts-accepted-in-nanophotonics-and-advanced-photonics-research",
